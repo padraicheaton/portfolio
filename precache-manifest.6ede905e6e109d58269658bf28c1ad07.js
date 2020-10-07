ea52e72b1a90243065e727f538cbd114a38d882b
pheaton264 <paddyheaton264@gmail.com>
2020-10-07 12:15:24 +1100

    Updates

---
+++ precache-manifest.6ede905e6e109d58269658bf28c1ad07.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32c518eceb9935757abab732c392be0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "080190416306ca908ebf",
+    "url": "/portfolio/static/css/main.325e35b3.chunk.css"
+  },
+  {
+    "revision": "adb77cc006e5c71ea252",
+    "url": "/portfolio/static/js/2.3a2ed7d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.3a2ed7d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "080190416306ca908ebf",
+    "url": "/portfolio/static/js/main.f05f153f.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "da848ee8d78bf19981045fe3e91e8f34",
+    "url": "/portfolio/static/media/deadline.da848ee8.png"
+  },
+  {
+    "revision": "0684486824e0dfcde0d90a5cb0d10622",
+    "url": "/portfolio/static/media/firstwebsite.06844868.png"
+  },
+  {
+    "revision": "f21dc44e2cf783964da174828f7e480a",
+    "url": "/portfolio/static/media/jasonassistant.f21dc44e.JPG"
+  },
+  {
+    "revision": "ed32053bf14b9f8a243b551b37f7974c",
+    "url": "/portfolio/static/media/mainbackground2.ed32053b.png"
+  },
+  {
+    "revision": "d12a505ba09143903769843478a945b5",
+    "url": "/portfolio/static/media/resume.d12a505b.pdf"
+  },
+  {
+    "revision": "b543ab96a19d0dbbcbcc0d66f6c2de9e",
+    "url": "/portfolio/static/media/shapeshifter.b543ab96.png"
+  }
+]);
